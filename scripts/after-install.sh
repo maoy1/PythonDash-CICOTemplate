49b8084dce682c93b109b06c4ba661d67bdaf773
Yan Mao <y.mao@elsevier.com>
2024-01-22 22:48:40 +0100

    update

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip3 install -r /home/ec2-user/scripts/requirements.txt
-cd /home/ec2-user/
-/bin/bash ./scripts/prepair_data.sh data/jenkins_jobs.csv data/job_details.csv
+
+/bin/bash /home/ec2-user/scripts/prepair_data.sh /home/ec2-user/data/jenkins_jobs.csv /home/ec2-user/data/job_details.csv
